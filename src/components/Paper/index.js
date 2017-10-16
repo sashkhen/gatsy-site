a48741f14c3ebbca04d7c7088e52f5c419d616d4
Aleksandra Diachenko <aleksandra.diachenko@yalantis.com>
2017-10-16 12:47:27 +0300

    Test partials and components

---
+++ src/components/Paper/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Paper = styled.div`
+  color: red;
+`;
+
+export default Paper;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,36 +3,9 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import Header from './Header'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import './index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
