a378656b00abe1c145c08eaa3297e6737dbf97f5
Aleksandra Diachenko <aleksandra.diachenko@yalantis.com>
2017-10-18 23:35:31 +0300

    Create markdown pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,60 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react"
+import styled from 'styled-components';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-  </div>
-)
+const Heading = styled.h1`
+  display: inline-block;
+  border-bottom: 1px solid;
+`;
 
-export default IndexPage
+const ListItem = styled.div`
+  margin-bottom: 10px;
+`;
+
+const DateText = styled.span`
+  color: #BBB;
+`;
+
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <div>
+      <Heading>Amazing Pandas Eating Things</Heading>
+      <h4>
+        {data.allMarkdownRemark.totalCount} Posts
+      </h4>
+      {data.allMarkdownRemark.edges.map(({ node }) =>
+        <div key={node.id}>
+          <ListItem>
+            {node.frontmatter.title}{" "}
+            <DateText>— {node.frontmatter.date}</DateText>
+          </ListItem>
+          <p>{node.excerpt}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: {
+      fields: [
+        frontmatter___date
+      ],
+      order: DESC
+    }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
